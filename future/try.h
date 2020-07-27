4c0396fb8a89787de20190d50bd33225f12a2a87
qicosmos <qicosmos@163.com>
2020-07-27 17:13:02 +0800

    remove unused code.

--- future/try.h
+++ future/try.h
@@ -144,57 +144,5 @@ namespace ray{
 
     template<typename T>
     using try_type_t = typename TryWrapper<T>::type;
-
-#if __cplusplus <= 201402L
-  template<typename R, typename F, typename Args>
-  absl::enable_if_t<std::is_void<R>::value, Try<void>> CallTryImpl(F&& func, Args&& args) {
-    std::forward<F>(func)(std::forward<Args>(args));
-    return Try<void>();
-  }
-
-  template<typename R, typename F>
-  absl::enable_if_t<std::is_void<R>::value, Try<void>> CallTryImpl(F&& func){
-    std::forward<F>(func)();
-    return Try<void>();
-  }
-
-  template<typename R, typename F, typename Args>
-  absl::enable_if_t<!std::is_void<R>::value, try_type_t<typename std::result_of<F(Args)>::type>>
-  CallTryImpl(F&& func, Args&& args) {
-    using Type = typename std::result_of<F(Args)>::type;
-    return try_type_t<Type>(std::forward<F>(func)(std::forward<Args>(args)));
-  }
-
-  template<typename R, typename F>
-  absl::enable_if_t<!std::is_void<R>::value, try_type_t<typename std::result_of<F()>::type>>
-  CallTryImpl(F&& func) {
-    using Type = typename std::result_of<F()>::type;
-    return try_type_t<Type>(std::forward<F>(func)());
-  }
-
-#endif
-
-  template<typename F, typename... Args>
-    auto CallTry(F&& func, Args&&... args) -> try_type_t<typename std::result_of<F(Args...)>::type> {
-    using Type = typename std::result_of<F(Args...)>::type;
-    using First = typename function_traits<F>::first_arg_t;
-
-    try {
-#if __cplusplus <= 201402L
-      return CallTryImpl<Type>(std::forward<F>(func),
-                               std::forward<Args>(args)...);
-#else
-        if constexpr (std::is_void<Type>::value) {
-          std::forward<F>(func)(std::forward<Args>(args)...);
-          return Try<void>();
-        } else {
-          return try_type_t<Type>(
-              std::forward<F>(func)(std::forward<Args>(args)...));
-        }
-#endif
-    } catch (std::exception &e) {
-      return try_type_t<Type>(std::current_exception());
-    }
-  }
 }
 #endif //FUTURE_DEMO_TRY_H
