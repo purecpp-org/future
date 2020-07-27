62bd4954bbb705cff96439a0476e23fe74028a58
qicosmos <qicosmos@163.com>
2020-07-27 17:37:41 +0800

    support test coverage for clion

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,10 +4,21 @@
 
 using namespace ray;
 
-TEST(test_my_class, get_age)
+int test(int* p){
+  if(p==nullptr){
+    return -1;
+  }
+
+  if(*p==1){
+    return 1;
+  }else{
+    return -2;
+  }
+}
+
+TEST(dummy, dummy_test)
 {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(3 > 0);
+  EXPECT_EQ(test(nullptr), -1);
 }
 
 int main(int argc, char **argv) {
